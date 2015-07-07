6946537c6d552b50920953fc680255e234d0b807
tnoumess <moume55i@yahoo.fr>
2015-07-07 00:02:57 -0400

    Some clean-up

--- WebContent/ngapp/app.js
+++ WebContent/ngapp/app.js
@@ -372,21 +372,7 @@ app.controller('removeController', function ($scope,FetchdataService,$http) {
 /*Begin*/
 app.controller('listcontroller', function ($scope,FetchdataService,$interval) {
 	$interval(function(){  FetchdataService.getStudents().success(function(data) {
-	       //this will execute when the 
-	       //AJAX call completes.
-	       $scope.students=data;
-	     /*  $scope.student=[
-	            	       {name:'John', age:25, gender:'boy'},
-	            	       {name:'Jessie', age:30, gender:'girl'},
-	            	       {name:'Johanna', age:28, gender:'girl'},
-	            	       {name:'Joy', age:15, gender:'girl'},
-	            	       {name:'Mary', age:28, gender:'girl'},
-	            	       {name:'Peter', age:95, gender:'boy'},
-	            	       {name:'Sebastian', age:50, gender:'boy'},
-	            	       {name:'Erika', age:27, gender:'girl'},
-	            	       {name:'Patrick', age:40, gender:'boy'},
-	            	       {name:'Samantha', age:60, gender:'girl'}
-	            	     ];*/
+	       
 	       console.log(data);
 	       console.log('innn');
 	       
